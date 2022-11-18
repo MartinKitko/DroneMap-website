a721001a50cf674eb006a9b32b5ccd59ba33754e
MartinKitko <martin.kitko736@gmail.com>
2022-11-18 20:02:13 +0100

    pridaný model pre marker

---
+++ App/Models/Marker.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Models;
+
+use App\Core\Model;
+
+class Marker extends Model
+{
+    protected ?int $id = 0;
+    protected ?string $title = "";
+    protected ?string $description = "";
+    protected ?float $lat = 0.0;
+    protected ?float $long = 0.0;
+
+    /**
+     * @return int|null
+     */
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int|null $id
+     */
+    public function setId(?int $id): void
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    /**
+     * @param string|null $title
+     */
+    public function setTitle(?string $title): void
+    {
+        $this->title = $title;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    /**
+     * @param string|null $description
+     */
+    public function setDescription(?string $description): void
+    {
+        $this->description = $description;
+    }
+
+    /**
+     * @return float|null
+     */
+    public function getLat(): ?float
+    {
+        return $this->lat;
+    }
+
+    /**
+     * @param float|null $lat
+     */
+    public function setLat(?float $lat): void
+    {
+        $this->lat = $lat;
+    }
+
+    /**
+     * @return float|null
+     */
+    public function getLong(): ?float
+    {
+        return $this->long;
+    }
+
+    /**
+     * @param float|null $long
+     */
+    public function setLong(?float $long): void
+    {
+        $this->long = $long;
+    }
+
+
+}
