fe6505967072a215e71525c50b7ffdecbfe5df2d
MartinKitko <martin.kitko736@gmail.com>
2022-11-19 16:46:26 +0100

    pridanie fukncionality pre pridávanie, úprava a mazanie markerov

--- App/Controllers/MarkersController.php
+++ App/Controllers/MarkersController.php
@@ -13,4 +13,57 @@ class MarkersController extends AControllerBase
         $markers = Marker::getAll();
         return $this->html($markers);
     }
+
+    public function delete()
+    {
+        $marker = Marker::getOne($this->request()->getValue('id'));
+        if ($marker) {
+            $marker->delete();
+        }
+
+        return $this->redirect('?c=markers');
+    }
+
+    /**
+     * @return \App\Core\Responses\ViewResponse
+     * @throws \Exception
+     */
+    public function edit()
+    {
+        return $this->html([
+            'marker' => Marker::getOne($this->request()->getValue('id'))
+        ],
+            'create.form'
+        );
+    }
+
+    /**
+     * @return \App\Core\Responses\ViewResponse
+     */
+    public function create()
+    {
+        return $this->html([
+            'marker' => new Marker()
+        ],
+            'create.form'
+        );
+    }
+
+    /**
+     * @return \App\Core\Responses\RedirectResponse
+     * @throws \Exception
+     */
+    public function store()
+    {
+        $id = $this->request()->getValue('id');
+        $marker = ($id ? Marker::getOne($id) : new Marker());
+
+        $marker->setTitle($this->request()->getValue("title"));
+        $marker->setDescription($this->request()->getValue("description"));
+        $marker->setLat($this->request()->getValue("lat"));
+        $marker->setLong($this->request()->getValue("long"));
+        $marker->save();
+
+        return $this->redirect("?c=markers");
+    }
 }
