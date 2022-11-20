0c5252461ce4ff47def0af4fe5d78d3b966e3a39
MartinKitko <martin.kitko736@gmail.com>
2022-11-20 13:13:38 +0100

    zmena farby textu

--- App/Views/Admin/index.view.php
+++ App/Views/Admin/index.view.php
@@ -2,7 +2,7 @@
 
 <div class="container-fluid">
     <div class="row">
-        <div class="col">
+        <div class="col text-white">
             <div>
                 Vitaj, <strong><?= $auth->getLoggedUserName() ?></strong>!<br><br>
                 Táto časť aplikácie je prístupná len po prihlásení.
