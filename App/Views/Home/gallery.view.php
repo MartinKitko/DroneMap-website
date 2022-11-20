6f29a581949bae385fc8a56d8607a44e4bfbc14b
MartinKitko <martin.kitko736@gmail.com>
2022-11-20 14:40:05 +0100

    zmena cesty k obrázkom

--- App/Views/Home/gallery.view.php
+++ App/Views/Home/gallery.view.php
@@ -1,63 +1,63 @@
 <?php /** @var Array $data */ ?>
 <div class="pics">
-    <a href="public/images/dr1.jpg" class="card-s" target="_blank">
-        <img src="public/images/dr1.jpg" alt="dr1">
+    <a href="public/images/gallery/dr1.jpg" class="card-s" target="_blank">
+        <img src="public/images/gallery/dr1.jpg" alt="dr1">
     </a>
-    <a href="public/images/dr2.jpg" class="card-s" target="_blank">
-        <img src="public/images/dr2.jpg" alt="dr2">
+    <a href="public/images/gallery/dr2.jpg" class="card-s" target="_blank">
+        <img src="public/images/gallery/dr2.jpg" alt="dr2">
     </a>
-    <a href="public/images/dr3.jpg" class="card-s" target="_blank">
-        <img src="public/images/dr3.jpg" alt="dr3">
+    <a href="public/images/gallery/dr3.jpg" class="card-s" target="_blank">
+        <img src="public/images/gallery/dr3.jpg" alt="dr3">
     </a>
-    <a href="public/images/dr4.jpg" class="card-s" target="_blank">
-        <img src="public/images/dr4.jpg" alt="dr4">
+    <a href="public/images/gallery/dr4.jpg" class="card-s" target="_blank">
+        <img src="public/images/gallery/dr4.jpg" alt="dr4">
     </a>
-    <a href="public/images/dr5.jpg" class="card-s" target="_blank">
-        <img src="public/images/dr5.jpg" alt="dr5">
+    <a href="public/images/gallery/dr5.jpg" class="card-s" target="_blank">
+        <img src="public/images/gallery/dr5.jpg" alt="dr5">
     </a>
-    <a href="public/images/dr6.jpg" class="card-s" target="_blank">
-        <img src="public/images/dr6.jpg" alt="dr6">
+    <a href="public/images/gallery/dr6.jpg" class="card-s" target="_blank">
+        <img src="public/images/gallery/dr6.jpg" alt="dr6">
     </a>
-    <a href="public/images/dr7.jpg" class="card-s" target="_blank">
-        <img src="public/images/dr7.jpg" alt="dr7">
+    <a href="public/images/gallery/dr7.jpg" class="card-s" target="_blank">
+        <img src="public/images/gallery/dr7.jpg" alt="dr7">
     </a>
-    <a href="public/images/dr8.jpg" class="card-s" target="_blank">
-        <img src="public/images/dr8.jpg" alt="dr8">
+    <a href="public/images/gallery/dr8.jpg" class="card-s" target="_blank">
+        <img src="public/images/gallery/dr8.jpg" alt="dr8">
     </a>
-    <a href="public/images/dr9.jpg" class="card-s" target="_blank">
-        <img src="public/images/dr9.jpg" alt="dr9">
+    <a href="public/images/gallery/dr9.jpg" class="card-s" target="_blank">
+        <img src="public/images/gallery/dr9.jpg" alt="dr9">
     </a>
-    <a href="public/images/dr10.jpg" class="card-s" target="_blank">
-        <img src="public/images/dr10.jpg" alt="dr10">
+    <a href="public/images/gallery/dr10.jpg" class="card-s" target="_blank">
+        <img src="public/images/gallery/dr10.jpg" alt="dr10">
     </a>
-    <a href="public/images/dr11.jpg" class="card-s" target="_blank">
-        <img src="public/images/dr11.jpg" alt="dr11">
+    <a href="public/images/gallery/dr11.jpg" class="card-s" target="_blank">
+        <img src="public/images/gallery/dr11.jpg" alt="dr11">
     </a>
-    <a href="public/images/dr12.jpg" class="card-s" target="_blank">
-        <img src="public/images/dr12.jpg" alt="dr12">
+    <a href="public/images/gallery/dr12.jpg" class="card-s" target="_blank">
+        <img src="public/images/gallery/dr12.jpg" alt="dr12">
     </a>
-    <a href="public/images/dr13.jpg" class="card-s" target="_blank">
-        <img src="public/images/dr13.jpg" alt="dr13">
+    <a href="public/images/gallery/dr13.jpg" class="card-s" target="_blank">
+        <img src="public/images/gallery/dr13.jpg" alt="dr13">
     </a>
-    <a href="public/images/dr14.jpg" class="card-s" target="_blank">
-        <img src="public/images/dr14.jpg" alt="dr14">
+    <a href="public/images/gallery/dr14.jpg" class="card-s" target="_blank">
+        <img src="public/images/gallery/dr14.jpg" alt="dr14">
     </a>
-    <a href="public/images/dr15.jpg" class="card-s" target="_blank">
-        <img src="public/images/dr15.jpg" alt="dr15">
+    <a href="public/images/gallery/dr15.jpg" class="card-s" target="_blank">
+        <img src="public/images/gallery/dr15.jpg" alt="dr15">
     </a>
-    <a href="public/images/dr16.jpg" class="card-s" target="_blank">
-        <img src="public/images/dr16.jpg" alt="dr16">
+    <a href="public/images/gallery/dr16.jpg" class="card-s" target="_blank">
+        <img src="public/images/gallery/dr16.jpg" alt="dr16">
     </a>
-    <a href="public/images/dr17.jpg" class="card-s" target="_blank">
-        <img src="public/images/dr17.jpg" alt="dr17">
+    <a href="public/images/gallery/dr17.jpg" class="card-s" target="_blank">
+        <img src="public/images/gallery/dr17.jpg" alt="dr17">
     </a>
-    <a href="public/images/dr18.jpg" class="card-s" target="_blank">
-        <img src="public/images/dr18.jpg" alt="dr18">
+    <a href="public/images/gallery/dr18.jpg" class="card-s" target="_blank">
+        <img src="public/images/gallery/dr18.jpg" alt="dr18">
     </a>
-    <a href="public/images/dr19.jpg" class="card-s" target="_blank">
-        <img src="public/images/dr19.jpg" alt="dr19">
+    <a href="public/images/gallery/dr19.jpg" class="card-s" target="_blank">
+        <img src="public/images/gallery/dr19.jpg" alt="dr19">
     </a>
-    <a href="public/images/dr20.jpg" class="card-s" target="_blank">
-        <img src="public/images/dr20.jpg" alt="dr20">
+    <a href="public/images/gallery/dr20.jpg" class="card-s" target="_blank">
+        <img src="public/images/gallery/dr20.jpg" alt="dr20">
     </a>
 </div>
